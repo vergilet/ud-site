8095be8fd405bd903d0fb272d3a752f02371ab7e
Yaro <osyaroslav@gmail.com>
2015-04-11 04:50:51 +0300

    enable iframes

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,10 @@ module UdSite
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
 
+    config.action_dispatch.default_headers = {
+        'X-Frame-Options' => 'ALLOWALL'
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
