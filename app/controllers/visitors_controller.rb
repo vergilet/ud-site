fce64ed70d93d239881c4c432be7b9c84e9f909b
Yaro <osyaroslav@gmail.com>
2015-07-20 23:05:26 +0300

    remove reverse

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,7 +4,7 @@ class VisitorsController < ApplicationController
 
   def index
     @series = Series.all_ordered_by_child.to_a.paginate(page: params[:page], per_page: 9)
-    @series_presenter = SeriesPresenter.instantiate(@series.reverse)
+    @series_presenter = SeriesPresenter.instantiate(@series)
     respond_to do |format|
       format.html
       format.js
