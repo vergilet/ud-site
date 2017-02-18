63d8a63ae3e7651adcad8e53b651af709edc095f
Yaro <vergilet@users.noreply.github.com>
2017-02-18 19:30:52 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
 constraints(host: /^(?!www\.)/i) do
-  get '' => redirect { |params, request|
+  match '' => redirect { |params, request|
     URI.parse(request.url).tap { |uri| uri.host = "www.#{uri.host}" }.to_s
   },  via: [:get, :post]
 end
