6c721807f1c92d68301ff914da25c4f147649107
Yaro <vergilet@users.noreply.github.com>
2017-02-18 19:37:15 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   
 constraints(host: /^(?!www\.)/i) do
-  match '' => redirect { |params, request|
+  get '/' => redirect { |params, request|
     URI.parse(request.url).tap { |uri| uri.host = "www.#{uri.host}" }.to_s
-  },  via: [:get, :post]
+  }
 end
 
   # TODO /admin/comments-management
