e83c508372f0e63e32e8ad2f890ada41a2a94a98
YaroslavO <yaroslavo@softwareplanetgroup.com>
2018-11-13 14:54:35 +0200

    fixes for meta player

--- config/initializers/video_info.rb
+++ config/initializers/video_info.rb
@@ -1,25 +1,4 @@
-VideoInfo::PROVIDERS.push('Meta')
-VideoInfo::PROVIDERS.push('FS')
-
-
-VideoInfo::Providers::Vkontakte.class_eval do
-
-  def thumbnail_small
-    _video_thumbnail(0)
-  end
-
-  def thumbnail_medium
-    _video_thumbnail(1)
-  end
-
-  def thumbnail_large
-    _video_thumbnail(2)
-  end
-
-  private
-
-  def _video_thumbnail(id)
-    '//placehold.it/85x70/5050ff/ffffff&text=Vk.com'
-  end
-
-end
+VideoInfo::PROVIDERS = %w[
+  Dailymotion Wistia
+  Vimeo Youtube YoutubePlaylist Meta
+]
